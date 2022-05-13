66176d245c77f5948f649093444bcd72f2e7bcc1
blackcommando1301 <100750703+blackcommando1301@users.noreply.github.com>
2022-05-13 14:07:03 +0530

    tic toe Game Completed

---
+++ kata.js
@@ -0,0 +1,130 @@
+let gameBox=document.querySelectorAll(".box");
+let textBox=document.querySelectorAll(".boxtext");
+let playerTurn=document.querySelector(".turn");
+let resetButton=document.querySelector(".reset");
+let imgPos=document.querySelector(".imge");
+let victoryImage=document.createElement("img");
+ victoryImage.src="./victory.gif";
+
+
+
+
+// let win=[ [0,1,2],[3,4,5],[6,7,8],[0,4,8],[2,4,6],[0,3,6],[1,4,7],[2,5,8]];
+
+
+let point="X";
+for(let i=0;i<gameBox.length;i++){
+gameBox[i].addEventListener('click',function(){
+    
+   textBox[i].innerHTML=point;
+   if(point=="X"){
+       point="0";
+       playerTurn.innerHTML="TURN OF 0"
+   }else{
+       point="X"
+       playerTurn.innerHTML="TURN OF X"
+   }
+    let zero =textBox[0].innerHTML;
+    let one =textBox[1].innerHTML;
+    let two =textBox[2].innerHTML;
+
+    let three =textBox[3].innerHTML;
+    let four =textBox[4].innerHTML;
+    let five =textBox[5].innerHTML;
+
+    let six =textBox[6].innerHTML;
+    let seven =textBox[7].innerHTML;
+    let eight =textBox[8].innerHTML;
+
+    // console.log(zero);
+    // console.log(one);
+    // console.log(two);
+    // console.log(three);
+    // console.log(four);
+    // console.log(five);
+    // console.log(six);
+    // console.log(seven);
+    // console.log(eight);
+
+    if((zero=="X" && one=="X"&& two=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((zero=="0" && one=="0"&& two=="0")){
+        playerTurn.innerHTML="Player 0 won the game "
+        imgPos.appendChild(victoryImage);
+        return; 
+    }else if((three=="X" && four=="X"&& five=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((three=="0" && four=="0"&& five=="0")){
+        playerTurn.innerHTML="Player 0 won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((six=="X" && seven=="X"&& eight=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((six=="0" && seven=="0"&& eight=="0")){
+        playerTurn.innerHTML="Player 0 won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((zero=="X" && four=="X"&& eight=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((zero=="0" && four=="0"&& eight=="0")){
+        playerTurn.innerHTML="Player 0 won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((two=="X" && four=="X"&& six=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((two=="0" && four=="0"&& six=="0")){
+        playerTurn.innerHTML="Player 0 won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((zero=="X" && three=="X"&& six=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((zero=="0" && three=="0"&& six=="0")){
+        playerTurn.innerHTML="Player 0 won the game "
+        imgPos.appendChild(victoryImage); 
+        return;
+    }else if((one=="X" && four=="X"&& seven=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((one=="0" && four=="0"&& seven=="0")){
+        playerTurn.innerHTML="Player 0 won the game "
+        imgPos.appendChild(victoryImage);
+        return; 
+    }else if((two=="X" && five=="X"&& eight=="X") ){
+        playerTurn.innerHTML="Player X won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }else if((two=="0" && five=="0"&& eight=="0")){
+        playerTurn.innerHTML="Player 0 won the game " 
+        imgPos.appendChild(victoryImage);
+        return;
+    }
+    
+    
+    
+   
+})
+
+
+
+resetButton.addEventListener("click",function(){
+    for(let i=0;i<textBox.length;i++){
+        textBox[i].innerHTML="";
+    }
+    playerTurn.innerHTML="TURN OF X"
+    victoryImage.src="";
+})
+
+}
